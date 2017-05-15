e9a07444ced9ab3ad8c3facd9f9ebba10b59259b
JSY2312 <siyoung>
2017-05-15 17:13:24 +0900

    DB테이블 클래스 객체화

---
+++ Project/Android/databaseTableSource/company/registration/ObjectTable.java
@@ -0,0 +1,33 @@
+package kr.co.company.registration;
+
+import java.io.Serializable;
+import java.sql.ResultSet;
+
+public abstract class ObjectTable implements Serializable{
+	String orderOperation;//디비가 동작해야할 동작을 지정
+	String orderTable;
+	String deleteRequest;
+	String insertRequest;
+	String updateRequest;
+	String selectRequest;
+	boolean resultResponse;//서버가 실행한 결과에 대한 성공 또는 실패 여부를 반환
+
+    private static final long serialVersionUID=2L;
+
+    public abstract String getOrderTable();
+    public abstract String getInsertRequest();
+    public abstract String getSelectRequest();
+    public abstract String getUpdateRequest();
+    public abstract String getDeleteRequest();
+    public abstract String getOrderOperation();    //INSERT ,UPDATE, SELECT,DELETE 지정하는 역할을 반환하는 메소드
+    public abstract boolean getResultResponse();
+
+    public abstract void setUpdateRequest(String str);
+    public abstract void setSelectRequest(String str);
+    public abstract void setInsertRequest(String str);
+    public abstract void setDeleteRequest(String str);
+    public abstract void setOrderOperation(String str);    //INSERT ,UPDATE, SELECT,DELETE 지정하는 역할을 수행하는 메소드
+    public abstract void setOrderTable(String str);
+    public abstract void setResultResponse(boolean result);
+    public abstract void setSelectOperation(ResultSet rs);
+}
---
+++ Project/Android/databaseTableSource/company/registration/WAYPOINT.java
@@ -0,0 +1,144 @@
+package kr.co.company.registration;
+
+import java.io.Serializable;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+class WAYPOINT extends ObjectTable implements Serializable{
+    private static final long serialVersionUID=2L;
+	private String CARPOOL_SERIAL_NUMBER;
+    private int WAYPOINT_ORDER;
+    private double WAYPOINT_LATITUDE;
+    private double WAYPOINT_LONGITUDE;
+    
+    
+    public String getCARPOOL_SERIAL_NUMBER() {
+		return CARPOOL_SERIAL_NUMBER;
+	}
+
+	public void setCARPOOL_SERIAL_NUMBER(String cARPOOL_SERIAL_NUMBER) {
+		this.CARPOOL_SERIAL_NUMBER = cARPOOL_SERIAL_NUMBER;
+	}
+
+	public int getWAYPOINT_ORDER() {
+		return WAYPOINT_ORDER;
+	}
+
+	public void setWAYPOINT_ORDER(int wAYPOINT_ORDER) {
+		this.WAYPOINT_ORDER = wAYPOINT_ORDER;
+	}
+
+	public double getWAYPOINT_LATITUDE() {
+		return WAYPOINT_LATITUDE;
+	}
+
+	public void setWAYPOINT_LATITUDE(double wAYPOINT_LATITUDE) {
+		this.WAYPOINT_LATITUDE = wAYPOINT_LATITUDE;
+	}
+
+	public double getWAYPOINT_LONGITUDE() {
+		return WAYPOINT_LONGITUDE;
+	}
+
+	public void setWAYPOINT_LONGITUDE(double wAYPOINT_LONGITUDE) {
+		this.WAYPOINT_LONGITUDE = wAYPOINT_LONGITUDE;
+	}
+
+    
+	@Override
+	public String getOrderTable() {
+		// TODO Auto-generated method stub
+		return this.orderTable;
+	}
+
+	@Override
+	public String getInsertRequest() {
+		// TODO Auto-generated method stub
+		return this.insertRequest;
+	}
+
+	@Override
+	public String getSelectRequest() {
+		// TODO Auto-generated method stub
+		return this.selectRequest;
+	}
+
+	@Override
+	public String getUpdateRequest() {
+		// TODO Auto-generated method stub
+		return this.updateRequest;
+	}
+
+	@Override
+	public String getDeleteRequest() {
+		// TODO Auto-generated method stub
+		return this.deleteRequest;
+	}
+
+	@Override
+	public String getOrderOperation() {
+		// TODO Auto-generated method stub
+		return this.orderOperation;
+	}
+
+	@Override
+	public boolean getResultResponse() {
+		// TODO Auto-generated method stub
+		return this.resultResponse;
+	}
+
+	@Override
+	public void setUpdateRequest(String str) {
+		// TODO Auto-generated method stub
+		this.updateRequest=str;
+	}
+
+	@Override
+	public void setSelectRequest(String str) {
+		// TODO Auto-generated method stub
+		this.selectRequest=str;
+	}
+
+	@Override
+	public void setInsertRequest(String str) {
+		// TODO Auto-generated method stub
+		this.insertRequest=str;
+	}
+
+	@Override
+	public void setDeleteRequest(String str) {
+		// TODO Auto-generated method stub
+		this.deleteRequest=str;
+	}
+
+	@Override
+	public void setOrderOperation(String str) {
+		// TODO Auto-generated method stub
+		this.orderOperation=str;
+	}
+
+	@Override
+	public void setOrderTable(String str) {
+		// TODO Auto-generated method stub
+		this.orderTable=str;
+	}
+
+	@Override
+	public void setResultResponse(boolean result) {
+		// TODO Auto-generated method stub
+		this.resultResponse=result;
+	}
+
+	@Override
+	public void setSelectOperation(ResultSet rs) {
+		// TODO Auto-generated method stub
+		 try {
+			 	this.setCARPOOL_SERIAL_NUMBER(rs.getString("CARPOOL_SERIAL_NUMBER"));
+				this.setWAYPOINT_ORDER(rs.getInt("WAYPOINT_ORDER"));
+			    this.setWAYPOINT_LATITUDE(rs.getDouble("WAYPOINT_LATITUDE"));
+			    this.setWAYPOINT_LONGITUDE(rs.getDouble("WAYPOINT_LONGITUDE"));
+	        } catch (SQLException e) {
+	            e.printStackTrace();
+	        }
+	}
+}
